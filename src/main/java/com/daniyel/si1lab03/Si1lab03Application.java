72a7ccd71a8a31d8448d1764e5392651b393bcca
Daniyel <daniyel.rocha@ccc.ufcg.edu.br>
2017-01-29 21:08:12 -0300

    Commit inicial do lab03

---
+++ src/main/java/com/daniyel/si1lab03/Si1lab03Application.java
@@ -0,0 +1,12 @@
+package com.daniyel.si1lab03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si1lab03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Si1lab03Application.class, args);
+	}
+}
---
+++ src/main/java/com/daniyel/si1lab03/ws/model/Tarefa.java
@@ -0,0 +1,9 @@
+package com.daniyel.si1lab03.ws.model;
+
+/**
+ * Created by Daniyel on 29/01/2017.
+ */
+public class Tarefa {
+
+
+}
