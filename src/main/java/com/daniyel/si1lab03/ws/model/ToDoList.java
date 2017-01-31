06dfb39218450c3ff987195da3986ec19fd45487
Daniyel <daniyel.rocha@ccc.ufcg.edu.br>
2017-01-31 00:40:43 -0300

    Criação e implementação de ToDoListController. Criação de ToDoListService. Criação de entidade ToDoList.

---
+++ src/main/java/com/daniyel/si1lab03/ws/model/ToDoList.java
@@ -0,0 +1,13 @@
+package com.daniyel.si1lab03.ws.model;
+
+import java.util.List;
+
+/**
+ * Created by Daniyel on 31/01/2017.
+ */
+public class ToDoList {
+    private long id;
+    private String title;
+    private List<Task> tasks;
+
+}
---
+++ src/main/java/com/daniyel/si1lab03/ws/service/ToDoListService.java
@@ -0,0 +1,26 @@
+package com.daniyel.si1lab03.ws.service;
+
+import com.daniyel.si1lab03.ws.model.ToDoList;
+
+import java.util.Collection;
+
+/**
+ * Created by Daniyel on 31/01/2017.
+ */
+public class ToDoListService {
+    public ToDoList getToDoListById(long id) {
+    }
+
+
+    public Collection<ToDoList> getAllToDoLists() {
+    }
+
+    public void createToDoList(ToDoList toDoList) {
+    }
+
+    public boolean deleteToDoList(long id) {
+    }
+
+    public ToDoList updateToDoList(ToDoList toDoList) {
+    }
+}
