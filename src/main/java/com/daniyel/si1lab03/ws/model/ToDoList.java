02f1bbc1cc505aa71858ff09d760e998918e4ae9
Daniyel <daniyel.rocha@ccc.ufcg.edu.br>
2017-01-31 00:49:33 -0300

    Alteração entidade ToDoList. Conclusão ToDoListService.

--- src/main/java/com/daniyel/si1lab03/ws/model/ToDoList.java
+++ src/main/java/com/daniyel/si1lab03/ws/model/ToDoList.java
@@ -10,4 +10,36 @@ public class ToDoList {
     private String title;
     private List<Task> tasks;
 
+    public ToDoList() {
+    }
+
+    public ToDoList(long id, String title, List<Task> tasks) {
+        this.id = id;
+        this.title = title;
+        this.tasks = tasks;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
